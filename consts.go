2e7f06f5d6524ae8e8286e573f00d850343a5486
A. Mogilevskiy <dark.qvark@gmail.com>
2021-08-14 02:31:26 +0300

    feat (core) Pin upd (2)

--- consts.go
+++ consts.go
@@ -1,29 +1,29 @@
 package gonvdgpio
 
 const (
-	GPIO_216 = 216
-	GPIO_50  = 50
-	GPIO_14  = 14
-	GPIO_194 = 194
-	GPIO_16  = 16
-	GPIO_17  = 17
-	GPIO_18  = 18
-	GPIO_149 = 149
-	GPIO_200 = 200
-	GPIO_38  = 38
-	GPIO_76  = 76
-	GPIO_12  = 12
+	gpioSysfsNumber216 = 216
+	gpioSysfsNumber50  = 50
+	gpioSysfsNumber14  = 14
+	gpioSysfsNumber194 = 194
+	gpioSysfsNumber16  = 16
+	gpioSysfsNumber17  = 17
+	gpioSysfsNumber18  = 18
+	gpioSysfsNumber149 = 149
+	gpioSysfsNumber200 = 200
+	gpioSysfsNumber38  = 38
+	gpioSysfsNumber76  = 76
+	gpioSysfsNumber12  = 12
 
-	GPIO_79  = 79
-	GPIO_232 = 232
-	GPIO_15  = 15
-	GPIO_13  = 13
-	GPIO_19  = 19
-	GPIO_20  = 20
-	GPIO_168 = 168
-	GPIO_51  = 51
-	GPIO_77  = 77
-	GPIO_78  = 78
+	gpioSysfsNumber79  = 79
+	gpioSysfsNumber232 = 232
+	gpioSysfsNumber15  = 15
+	gpioSysfsNumber13  = 13
+	gpioSysfsNumber19  = 19
+	gpioSysfsNumber20  = 20
+	gpioSysfsNumber168 = 168
+	gpioSysfsNumber51  = 51
+	gpioSysfsNumber77  = 77
+	gpioSysfsNumber78  = 78
 )
 
 const (
@@ -44,27 +44,27 @@ var gpioIndexNumberToSysfsNumberMap map[int]int
 
 func init() {
 	gpioIndexNumberToSysfsNumberMap = map[int]int{
-		7:  GPIO_216,
-		11: GPIO_50,
-		12: GPIO_79,
-		13: GPIO_14,
-		15: GPIO_194,
-		16: GPIO_232,
-		18: GPIO_15,
-		19: GPIO_16,
-		21: GPIO_17,
-		22: GPIO_13,
-		23: GPIO_18,
-		24: GPIO_19,
-		26: GPIO_20,
-		29: GPIO_149,
-		31: GPIO_200,
-		32: GPIO_168,
-		33: GPIO_38,
-		35: GPIO_76,
-		36: GPIO_51,
-		37: GPIO_12,
-		38: GPIO_77,
-		40: GPIO_78,
+		7:  gpioSysfsNumber216,
+		11: gpioSysfsNumber50,
+		12: gpioSysfsNumber79,
+		13: gpioSysfsNumber14,
+		15: gpioSysfsNumber194,
+		16: gpioSysfsNumber232,
+		18: gpioSysfsNumber15,
+		19: gpioSysfsNumber16,
+		21: gpioSysfsNumber17,
+		22: gpioSysfsNumber13,
+		23: gpioSysfsNumber18,
+		24: gpioSysfsNumber19,
+		26: gpioSysfsNumber20,
+		29: gpioSysfsNumber149,
+		31: gpioSysfsNumber200,
+		32: gpioSysfsNumber168,
+		33: gpioSysfsNumber38,
+		35: gpioSysfsNumber76,
+		36: gpioSysfsNumber51,
+		37: gpioSysfsNumber12,
+		38: gpioSysfsNumber77,
+		40: gpioSysfsNumber78,
 	}
 }
--- gpio.go
+++ gpio.go
@@ -16,43 +16,43 @@ type Pin struct {
 func SetupPin(indexNumber int, direction string, level int) (*Pin, error) {
 	sysfsNumber, ok := gpioIndexNumberToSysfsNumberMap[indexNumber]
 	if !ok {
-		return nil, fmt.Errorf("gonvdgpio[.SetupPin]: invalid pin number: %v", indexNumber)
+		return nil, fmt.Errorf("gonvdgpio[.SetupPin][1]: invalid pin number: %v", indexNumber)
 	}
 
 	pin := &Pin{indexNumber: indexNumber, sysfsNumber: sysfsNumber}
 
 	if err := pin.setup(); err != nil {
-		// TODO
+		return nil, fmt.Errorf("gonvdgpio[.SetupPin][2]: %v", indexNumber)
 	}
 
 	if err := pin.SetDirection(direction); err != nil {
-		// TODO
+		return nil, fmt.Errorf("gonvdgpio[.SetupPin][3]: %v", indexNumber)
 	}
 
 	if err := pin.SetLevel(level); err != nil {
-		// TODO
+		return nil, fmt.Errorf("gonvdgpio[.SetupPin][4]: %v", indexNumber)
 	}
 
 	return pin, nil
 }
 
-func (p *Pin) SetLevel(level int) error {
+func (p Pin) SetLevel(level int) error {
 	levelDirectory := path.Join(gpiosDir, p.getDirectoryName(), "value")
 
 	err := ioutil.WriteFile(levelDirectory, []byte(strconv.Itoa(level)), 0666)
 	if err != nil {
-		return fmt.Errorf("[Pin.SetLevel]: %+v", err)
+		return fmt.Errorf("gonvdgpio[Pin.SetLevel][1]: %+v", err)
 	}
 
 	return nil
 }
 
-func (p *Pin) GetLevel() (int, error) {
+func (p Pin) GetLevel() (int, error) {
 	levelDir := path.Join(gpiosDir, p.getDirectoryName(), "value")
 
 	content, err := ioutil.ReadFile(levelDir)
 	if err != nil {
-		return 0, fmt.Errorf("[Pin.GetLevel][1]: %+v", err)
+		return 0, fmt.Errorf("gonvdgpio[Pin.GetLevel][1]: %+v", err)
 	}
 
 	levelRaw := strings.TrimRight(string(content), "\n")
@@ -61,7 +61,7 @@ func (p *Pin) GetLevel() (int, error) {
 
 	level, err := strconv.Atoi(levelRaw)
 	if err != nil {
-		return 0, fmt.Errorf("[Pin.GetLevel][2]: %+v", err)
+		return 0, fmt.Errorf("gonvdgpio[Pin.GetLevel][2]: %+v", err)
 	}
 
 	switch level {
@@ -71,26 +71,26 @@ func (p *Pin) GetLevel() (int, error) {
 		return HIGH, nil
 	}
 
-	return 0, fmt.Errorf("[Pin.GetLevel][3]: undefined")
+	return 0, fmt.Errorf("gonvdgpio[Pin.GetLevel][3]: undefined")
 }
 
-func (p *Pin) SetDirection(direction string) error {
+func (p Pin) SetDirection(direction string) error {
 	directionPath := path.Join(gpiosDir, p.getDirectoryName(), "direction")
 
 	err := ioutil.WriteFile(directionPath, []byte(direction), 0666)
 	if err != nil {
-		return fmt.Errorf("[Pin.SetDirection]: %+v", err)
+		return fmt.Errorf("gonvdgpio[Pin.SetDirection][1]: %+v", err)
 	}
 
 	return nil
 }
 
-func (p *Pin) GetDirection() (string, error) {
+func (p Pin) GetDirection() (string, error) {
 	directionPath := path.Join(gpiosDir, p.getDirectoryName(), "direction")
 
 	content, err := ioutil.ReadFile(directionPath)
 	if err != nil {
-		return "", fmt.Errorf("[Pin.GetDirection]: %v", err)
+		return "", fmt.Errorf("gonvdgpio[Pin.GetDirection][1]: %v", err)
 	}
 
 	switch string(content) {
@@ -100,25 +100,17 @@ func (p *Pin) GetDirection() (string, error) {
 		return OUT, nil
 	}
 
-	return "", fmt.Errorf("[Pin.GetDirection]: undefined")
+	return "", fmt.Errorf("gonvdgpio[Pin.GetDirection][1]: undefined")
 }
 
-func ListPins() {
-	panic("implements")
-}
+// --- private
 
-func CleanUp() error {
-	panic("implements")
-}
-
-// ---
-
-func (p *Pin) setup() error {
+func (p Pin) setup() error {
 	filePath := path.Join(gpiosDir, "export")
 
 	err := ioutil.WriteFile(filePath, []byte(strconv.Itoa(p.sysfsNumber)), 0666)
 	if err != nil {
-		return fmt.Errorf("[Pin.setup]: %+v", err)
+		return fmt.Errorf("gonvdgpio[Pin.setup][1]: %+v", err)
 	}
 
 	return nil
