7f9dbb369af325a92f322d9ee576d18271a3b701
A. Mogilevskiy <dark.qvark@gmail.com>
2021-08-14 19:43:20 +0300

    test (core) add case_1

--- examples/case_1.go
+++ examples/case_1.go
@@ -1,5 +1,60 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
 
+	gonvdgpio "github.com/alexplusm/go-nvidia-gpio"
+)
+
+func main() {
+	var (
+		pinNumber int
+		direction string
+		level     int
+	)
+
+	flag.IntVar(&pinNumber, "pn", -1, "pin number")
+	flag.StringVar(&direction, "d", gonvdgpio.OUT, "direction")
+	flag.IntVar(&level, "l", -1, "level")
+	flag.Parse()
+
+	p, err := gonvdgpio.SetupPin(pinNumber, direction, level)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(time.Second * 2)
+
+	for {
+		if err = p.SetLevel(gonvdgpio.HIGH); err != nil {
+			fmt.Printf("Err: SetLevel: %+v", err)
+		}
+
+		printCurrState(p)
+		time.Sleep(time.Second * 2)
+
+		if err = p.SetLevel(gonvdgpio.LOW); err != nil {
+			fmt.Printf("Err: SetLevel: %+v", err)
+		}
+
+		printCurrState(p)
+		time.Sleep(time.Second * 2)
+	}
+}
+
+func printCurrState(pin *gonvdgpio.Pin) {
+	currLevel, err := pin.GetLevel()
+	if err != nil {
+		fmt.Printf("Err: GetLevel: %+v", err)
+	}
+
+	currDir, err := pin.GetDirection()
+	if err != nil {
+		fmt.Printf("Err: GetDirection: %+v", err)
+	}
+
+	fmt.Printf("Curr state: level = %v, direction = %v", currLevel, currDir)
 }
